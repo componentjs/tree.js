c25f4855be1a4d83645988bfac3f8c9b4eee7192
TJ Holowaychuk <tj@vision-media.ca>
2014-02-05 12:58:02 -0800

    refactor to get rid of pyramid of doom
    CALLBACKS ARE FINE THEY SAID

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ var join = path.join;
 /**
  * Load component tree from `path`.
  *
- * CALLBACKS ARE FINE OK
- *
  * @param {String} path
  * @param {Function} fn
  * @api public
@@ -23,61 +21,81 @@ var join = path.join;
 module.exports = function(path, fn){
   var tree = {};
 
-  var batch = new Batch;
-  batch.concurrency(8);
-
-  // ./<user>
-  readdir(path, function(err, users){
-    if (err) return fn(err);
-
-    users.forEach(function(user){
-      batch.push(function(done){
-        var dir = join(path, user);
-        tree[user] = tree[user] || {};
-
-        // ./<user>/<repo>
-        readdir(dir, function(err, pkgs){
-          if (err) return done(err);
-
-          pkgs.forEach(function(pkg){
-            tree[user][pkg] = tree[user][pkg] || {};
-
-            // ./<user>/<repo>/<versions>
-            readdir(join(dir, pkg), function(err, versions){
-              if (err) return done(err);
-              var b = new Batch;
-              b.concurrency(8);
-
-              versions.forEach(function(version){
-                b.push(function(done){
-
-                  // ./<user>/<repo>/<versions>/component.json
-                  var path = join(dir, pkg, version, 'component.json');
-
-                  read(path, 'utf8', function(err, json){
-                    if (err) return done(err);
-
-                    try {
-                      var obj = JSON.parse(json);
-                    } catch (err) {
-                      return done(err);
-                    }
-
-                    tree[user][pkg][version] = obj;
-                    done();
-                  });
-                })
-              });
-
-              b.end(done);
-            });
-          });
-        });
-      })
-    });
-
-    batch.end(function(err){
-      fn(err, tree);
-    });
+  users(tree, path, function(err){
+    fn(err, tree);
   });
 };
+
+/**
+ * ./components/<user>
+ */
+
+function users(tree, path, fn) {
+  readdir(path, function(err, users){
+    var batch = new Batch;
+    batch.concurrency(6);
+
+    users.forEach(function(user){
+      tree[user] = tree[user] || {};
+
+      batch.push(function(done){
+        repos(tree[user], join(path, user), done);
+      });
+    });
+
+    batch.end(fn);
+  });
+}
+
+/**
+ * ./components/<user>/<repo>
+ */
+
+function repos(tree, path, fn) {
+  readdir(path, function(err, repos){
+    var batch = new Batch;
+    batch.concurrency(6);
+
+    repos.forEach(function(repo){
+      tree[repo] = tree[repo] || {};
+
+      batch.push(function(done){
+        versions(tree[repo], join(path, repo), done);
+      });
+    });
+
+    batch.end(fn);
+  });
+}
+
+/**
+ * ./components/<user>/<repo>/<versions>
+ */
+
+function versions(tree, path, fn) {
+  readdir(path, function(err, versions){
+    var batch = new Batch;
+    batch.concurrency(6);
+
+    versions.forEach(function(version){
+      batch.push(function(done){
+        var file = join(path, version, 'component.json');
+
+        read(file, 'utf8', function(err, json){
+          if (err) return done(err);
+
+          try {
+            var obj = JSON.parse(json);
+          } catch (err) {
+            return done(err);
+          }
+
+          tree[version] = obj;
+          done();
+        });
+      });
+    });
+
+    batch.end(fn);
+  });
+}
